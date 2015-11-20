7223f69cb6e127df43453d2324f6b9f9e94ac70c
nicola <notsecurity@gmail.com>
2015-11-20 16:44:30 -0500

    making it compliant with 0.3

--- index.js
+++ index.js
@@ -1,59 +1,67 @@
-'use strict'
+module.exports = MultiStore
+var AbstractStore = require('rdf-store-abstract')
+var util = require('util')
 
 function MultiStore (options) {
   var self = this
 
   options = options || {}
   self.router = options.router
+}
+util.inherits(MultiStore, AbstractStore)
 
-  self.graph = function (iri, callback, options) {
-    self.router('graph', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.graph(iri, callback, options)
-    })
-  }
-
-  self.match = function (iri, subject, predicate, object, callback, limit, options) {
-    self.router('match', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.match(iri, subject, predicate, object, callback, limit, options)
-    })
-  }
-
-  self.add = function (iri, graph, callback, options) {
-    self.router('add', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.add(iri, graph, callback, options)
-    })
-  }
-
-  self.merge = function (iri, graph, callback, options) {
-    self.router('merge', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.merge(iri, graph, callback, options)
-    })
-  }
-
-  self.remove = function (iri, graph, callback, options) {
-    self.router('remove', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.remove(iri, graph, callback, options)
-    })
-  }
-
-  self.removeMatches = function (iri, subject, predicate, object, callback, options) {
-    self.router('removeMatches', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.removeMatches(iri, subject, predicate, object, callback, options)
-    })
-  }
-
-  self.delete = function (iri, callback, options) {
-    self.router('delete', arguments, function (err, store) {
-      if (err) return callback(null, err)
-      store.delete(iri, callback, options)
-    })
-  }
+MultiStore.prototype.graph = function (iri, callback, options) {
+  var self = this
+  self.router('graph', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.graph(iri, callback, options)
+  })
 }
 
-module.exports = MultiStore
+MultiStore.prototype.match = function (iri, subject, predicate, object, callback, limit, options) {
+  var self = this
+  self.router('match', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.match(iri, subject, predicate, object, callback, limit, options)
+  })
+}
+
+MultiStore.prototype.add = function (iri, graph, callback, options) {
+  var self = this
+  self.router('add', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.add(iri, graph, callback, options)
+  })
+}
+
+MultiStore.prototype.merge = function (iri, graph, callback, options) {
+  var self = this
+  self.router('merge', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.merge(iri, graph, callback, options)
+  })
+}
+
+MultiStore.prototype.remove = function (iri, graph, callback, options) {
+  var self = this
+  self.router('remove', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.remove(iri, graph, callback, options)
+  })
+}
+
+MultiStore.prototype.removeMatches = function (iri, subject, predicate, object, callback, options) {
+  var self = this
+  self.router('removeMatches', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.removeMatches(iri, subject, predicate, object, callback, options)
+  })
+}
+
+MultiStore.prototype.delete = function (iri, callback, options) {
+  var self = this
+  self.router('delete', arguments, function (err, store) {
+    if (err) return callback(err)
+    store.delete(iri, callback, options)
+  })
+}
